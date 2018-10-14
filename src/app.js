28adcf0aea826dde19197161e6292e1b7373c144
Justus Deitert <mail@justusdeitert.de>
2018-10-14 15:41:52 +0200

    Replace custom parallax with Materialize CSS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import './scss/styles.scss';
 
 // node_modules
 import 'materialize-css';
-import 'jquery-parallax.js';
+// import 'jquery-parallax.js';
 
 // js imports
 import './js/sendMail';
@@ -17,12 +17,13 @@ jQuery(function ($) {
     console.log('%c DOM Ready!!', 'font-weight: bold;');
     console.log('%c Hello Visitor! :)', 'color: green; font-weight: bold; font-size: 2em');
 
-    const sidenav = M.Sidenav.init(document.querySelector('.sidenav'), { edge: 'right' });
-    const scrollspy = M.ScrollSpy.init(document.querySelector('.scrollspy'));
-    const tabs = M.Tabs.init(document.querySelector('.tabs'), { swipeable: false });
-    // const materialbox = M.Materialbox.init(document.querySelector('.materialboxed'));
+    $('.sidenav').sidenav({ edge: 'right' });
+    $('.scrollspy').scrollSpy({ scrollOffset: 0});
+    $('.tabs').tabs({ swipeable: false });
+
 
     $(window).on('load', function() {
         $('body').removeClass('loading');
+        $('.parallax').parallax();
     });
 });
